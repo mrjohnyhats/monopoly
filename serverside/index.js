d0cc60b9ae2748d2a0e6a0179ec28bf4a3bc107f
mrjohnyhats <jonbutterminecraftian@gmail.com>
2017-04-21 16:35:26 -0700

    started to do boilerplate stuff

---
+++ serverside/index.js
@@ -0,0 +1,17 @@
+const {BrowserWindow, app} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+const createWindow = () => {
+    win = new BrowserWindow({width: 1000, height: 800});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '../clientside/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+};
+
+app.on('ready', createWindow);
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './clientside/start.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
